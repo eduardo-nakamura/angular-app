f1b5d7462faa902a92b2f746d36a0ae7a8d86d74
eduardo-nakamura <edu-nakamura@hotmail.com>
2019-11-06 16:57:58 -0300

    https://scrimba.com/p/pQxesM/cDEg4tM

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({ 
   selector: 'app-root',
-  template: `
-    <h1>{{title}}</h1>
-  `
+  templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
   title = 'teste'
