c8aa5e589f474794b7938b8a924c4b2c769a23e1
eduardo-nakamura <edu-nakamura@hotmail.com>
2019-11-08 16:30:19 -0300

    Data Binding in angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { SharedModule } from  './shared/shared.module';
+
 import { AppComponent } from './app.component';
 // import { CustomersComponent } from './customers/customers.component';
 import { CustomersModule } from './customers/customers.module';
 
 
 @NgModule({
-  imports:      [ BrowserModule, CustomersModule ],
+  imports:      [ BrowserModule, CustomersModule,SharedModule ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
